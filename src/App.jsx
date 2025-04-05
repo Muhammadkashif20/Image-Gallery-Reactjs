f4a4dbb642fc4f66f9fb73e57f22b8d2c0ce14f5
Muhammadkashif20 <154686104+Muhammadkashif20@users.noreply.github.com>
2025-04-06 00:28:26 +0500

    working on showing images using unsplash api and mui for better UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import Navbar from "./Components/Navbar";
+import { Box, Button, TextField } from "@mui/material";
+import {Masonry} from "@mui/lab";
+import { useState } from "react";
+import axios from "axios";
+import { Spin } from "antd";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(()=>{
+    const fetchImage=async ()=>{
+        setLoading(true)
+        const res=await axios.get("https://api.unsplash.com/photos/?client_id=d4Bi80zrWBao4b_1wtfYYaVpoLk1GvURLKr-hdY-jfY&per_page=30")
+        console.log("res=>",res.data)
+        setImages(res.data)
+        setLoading(false)
+    }
+    fetchImage()
+  },[])
+  console.log("Storeimages", images);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Box>
+        <Navbar />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            flexDirection: "row",
+            gap: 2,
+            my: "2rem",
+            mx: "2rem",
+          }}
+        >
+          <TextField
+            fullWidth
+            name="Search Your Image"
+            label="Search Your Image"
+          />
+          <Button variant="outlined" color="primary">
+            Search
+          </Button>
+        </Box>
+        {loading ? (
+    <Box sx={{display:"flex",justifyContent:"center",alignItems:"center",height:"50vh"}}>
+    <Spin size="large" tip="Loading..." />
+    </Box>
+      ) : null}
+      <Masonry columns={{sm:1 ,md:3,lg:4}} sx={{ my: "2rem",mx: "2rem"}}  >
+        {images.map((image) => (
+          <Box  key={image.id}>
+            <img
+              src={image.urls.small}
+              alt={image.alt_description}
+              style={{ width: "100%", height: "auto", borderRadius: "2px" }}
+            />
+          </Box>
+        ))}
+      </Masonry>
+      </Box>
+    </div>
+  );
+};
 
-export default App
+export default App;
